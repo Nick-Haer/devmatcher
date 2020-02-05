41e9354953fe2dd401f8517685b34647acb08471
Nick.Haer <nhaer123@gmail.com>
2020-02-05 15:59:22 -0600

    added redux, receiving error for response.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import NavBar from './components/layout/NavBar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import {
+  BrowserRouter as Router,
+  Route,
+  Swtich,
+  Switch,
+} from 'react-router-dom';
 import './App.css';
 
+import { Provider } from 'react-redux';
+import store from './store';
+import Alert from './components/layout/Alert';
+
 const App = () => (
-  <>
-    <h1>App</h1>
-  </>
+  <Provider store={store}>
+    <Router>
+      <>
+        <NavBar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Alert />
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </>
+    </Router>
+  </Provider>
 );
 
 export default App;
